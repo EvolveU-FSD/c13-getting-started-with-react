bd34864eb653e7987b27dcf98d02bab5a4ec3954
shaneinceptionu <shane@inceptionu.com>
2024-10-31 11:02:53 -0600

    hot garbage code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ReadTheDocs from './ReadTheDocs'
 
 function CountWidget({ valueToShow, suffix }) { 
+  // const candy = "apple";
+  // const [costume, setCostume] = useState();
+  const [candy, setCandy] = useState("apple");
+  // const [street, setStreet] = useState({address: "1t12g24g", trickOrTreat: true});
+  // setCostume("vampire")
+  // function swapCandy() {
+  //   if(candy === "apple"){
+  //     return "chocolate";
+  //   } else if(candy === "chocolate"){
+  //     return "apple";
+  //   }
+  // }
   return (
     <div>
       count is {valueToShow}, {suffix}
+      <p>I ate so much candy!: {candy}</p>
+      {/* <button onClick={() => let newCandy=swapCandy(); setCandy(newCandy)}> */}
+      <button onClick={() => setCandy(
+       candy==="apple"? "chocolate":candy==="chocolate"? "apple": "chocolate")}>
+        		Click me to add candy!
+      </button>
+
+      {/* <Halloween></Halloween> */}
     </div>
   )
 }
+
+// function Halloween(){
+//   return(
+//     <p>
+//       halloween is awesome {candy}
+//     </p>
+//   )
+// }
 
 function CountButton({ count, buttonIsClicked }) {
   return (
@@ -23,8 +51,19 @@ function CountButton({ count, buttonIsClicked }) {
 function App() {
   const [count, setCount] = useState(0)
 
-  function decrementCount() {  return setCount((count) => count - 1) }
+  function decrementCount() {  
+    setCount((count) => count - 1) 
+  }
+  
   const incrementCount = () => setCount((count) => count + 1)
+
+  useEffect(() => {
+    if (count > 10) {
+      document.body.style.backgroundColor = 'lightblue';
+    } else {
+      document.body.style.backgroundColor = '';
+    }
+  }, [count]); // Effect runs whenever 'count' changes
 
   return (
     <>
@@ -46,11 +85,6 @@ function App() {
           buttonIsClicked={decrementCount} 
         />
         <CountWidget valueToShow={count} suffix="man!"/>
-        <CountWidget valueToShow={3} suffix="woo hoo!"/>
-        <CountWidget valueToShow={count} />
-        <CountWidget valueToShow="hello there" />
-        <CountWidget valueToShow={count} />
-        <CountWidget valueToShow={count} />
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
